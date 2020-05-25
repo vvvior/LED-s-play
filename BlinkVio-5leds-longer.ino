046dd76aed5a0f776026be303eb7625503c48113
vvvior <65898558+vvvior@users.noreply.github.com>
2020-05-25 12:28:22 +0100

    Add files via upload

---
+++ BlinkVio-5leds-longer.ino
@@ -0,0 +1,158 @@
+
+//  An interesting light sequence, using all LEDs provided in starter kit.
+//  Most Arduinos have an on-board LED you can control. On the UNO, MEGA and ZERO
+//  it is attached to digital pin 13, on MKR1000 on pin 6. LED_BUILTIN is set to
+//  the correct LED pin independent of which board is used.
+ 
+ // created 25-05-2020
+ // by vvvior
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+  
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  pinMode(8, OUTPUT);// initialize digital pin 8 as an output.
+  digitalWrite(8, HIGH);// turn the LED on (HIGH is the voltage level)
+  delay(50);
+  digitalWrite(8, LOW);// turn the LED off by making the voltage LOW
+  delay(50);
+  digitalWrite(8, HIGH);
+  delay(50);
+  digitalWrite(8, LOW);
+  delay(50);
+  pinMode(9, OUTPUT);// initialize digital pin 9 as an output.
+  digitalWrite(9, HIGH);
+  delay(50);        
+  digitalWrite(9, LOW); 
+  delay(50);     
+  digitalWrite(9, HIGH);
+  delay(50); 
+  digitalWrite(9, LOW); 
+  delay(50);
+  pinMode(10, OUTPUT);// initialize digital pin 10 as an output.
+  digitalWrite(10, HIGH);
+  delay(50);
+  digitalWrite(10, LOW);
+  delay(50);
+  digitalWrite(10, HIGH);
+  delay(50);
+  digitalWrite(10, LOW);
+  delay(50);
+  pinMode(11, OUTPUT);// initialize digital pin 12 as an output.
+  digitalWrite(11, HIGH);
+  delay(50);
+  digitalWrite(11, LOW);
+  delay(50);
+  digitalWrite(11, HIGH);
+  delay(50);
+  digitalWrite(11, LOW);
+  delay(50);
+  pinMode(12, OUTPUT);
+  digitalWrite(12, HIGH);
+  delay(50);
+  digitalWrite(12, LOW);
+  delay(50);
+  digitalWrite(12, HIGH);
+  delay(50);
+  digitalWrite(12, LOW);
+  delay(50);
+  pinMode(8, OUTPUT);
+  digitalWrite(8, HIGH);
+  delay(50);
+  digitalWrite(8, LOW);
+  delay(50);
+  pinMode(9, OUTPUT);
+  digitalWrite(9, HIGH);
+  delay(50);      
+  digitalWrite(9, LOW);
+  delay(50);    
+  pinMode(10, OUTPUT);
+  digitalWrite(10, HIGH);
+  delay(50);
+  digitalWrite(10, LOW);
+  delay(50);
+  pinMode(11, OUTPUT);
+  digitalWrite(11, HIGH);
+  delay(50);
+  digitalWrite(11, LOW);
+  delay(50);
+  pinMode(12, OUTPUT);
+  digitalWrite(12, HIGH);
+  delay(50);
+  digitalWrite(12, LOW);
+  delay(50);
+  pinMode(8, OUTPUT);
+  digitalWrite(8, HIGH);
+  delay(20);
+  digitalWrite(8, LOW);
+  delay(50);
+  pinMode(9, OUTPUT);
+  digitalWrite(9, HIGH);
+  delay(20);             
+  digitalWrite(9, LOW);
+  delay(50);       
+  pinMode(10, OUTPUT);
+  digitalWrite(10, HIGH);
+  delay(20);
+  digitalWrite(10, LOW);
+  delay(50);
+  pinMode(11, OUTPUT);
+  digitalWrite(11, HIGH);
+  delay(20);
+  digitalWrite(11, LOW);
+  delay(50);
+  pinMode(12, OUTPUT);
+  digitalWrite(12, HIGH);
+  delay(20);
+  digitalWrite(12, LOW);
+  delay(50);
+  pinMode(8, OUTPUT);
+  digitalWrite(8, HIGH);
+  delay(10);
+  digitalWrite(8, LOW);
+  delay(50);
+  pinMode(9, OUTPUT);
+  digitalWrite(9, HIGH);
+  delay(10);         
+  digitalWrite(9, LOW);
+  delay(50);     
+  pinMode(10, OUTPUT);
+  digitalWrite(10, HIGH);
+  delay(10);
+  digitalWrite(10, LOW);
+  delay(50);
+  pinMode(11, OUTPUT);
+  digitalWrite(11, HIGH);
+  delay(10);
+  digitalWrite(11, LOW);
+  delay(50);
+  pinMode(12, OUTPUT);
+  digitalWrite(12, HIGH);
+  delay(10);
+  digitalWrite(12, LOW);
+  delay(50);
+  pinMode(8, OUTPUT);
+  digitalWrite(8, HIGH);
+  delay(10);
+  digitalWrite(8, LOW);
+  pinMode(9, OUTPUT);
+  digitalWrite(9, HIGH);
+  delay(10);         
+  digitalWrite(9, LOW);
+  pinMode(10, OUTPUT);
+  digitalWrite(10, HIGH);
+  delay(10);
+  digitalWrite(10, LOW);
+  pinMode(11, OUTPUT);
+  digitalWrite(11, HIGH);
+  delay(10);
+  digitalWrite(11, LOW);
+  pinMode(12, OUTPUT);
+  digitalWrite(12, HIGH);
+  delay(10);
+  digitalWrite(12, LOW);
+  delay(850); 
+}
